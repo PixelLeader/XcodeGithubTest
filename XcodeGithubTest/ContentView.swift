009ae1bb585140f80bb2f1c7c20df0a1d38a6382
test1 <test1@test.com>
2024-11-24 15:02:48 +0100

    Changed some text, because I am the best and F the world

--- XcodeGithubTest/ContentView.swift
+++ XcodeGithubTest/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Fuck you >>> world!")
+            Text("I am the best!")
         }
         .padding()
     }
