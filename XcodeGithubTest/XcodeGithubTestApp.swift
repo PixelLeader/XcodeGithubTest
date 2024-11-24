cc933471fb17db76089c3b372056687636bf191a
test1 <test1@test.com>
2024-11-24 14:50:39 +0100

    Initial Commit

---
+++ XcodeGithubTest/XcodeGithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGithubTestApp.swift
+//  XcodeGithubTest
+//
+//  Created by John Doe on 24/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
