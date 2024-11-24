2a8a566bb0493cd8317f72f6231d355258602d5b
test1 <test1@test.com>
2024-11-24 15:31:59 +0100

    Created a second view page

---
+++ XcodeGithubTest/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  XcodeGithubTest
+//
+//  Created by John Doe on 24/11/2024.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
